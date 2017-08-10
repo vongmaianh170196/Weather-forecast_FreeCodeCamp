50fb3dd4d4ce1db06bf9c76c8b9865007051f858
Anh <anh.vo@student.lamk.fi>
2017-08-10 15:46:05 +0300

    Function Getgeolocation v1

--- app.js
+++ app.js
@@ -4,15 +4,42 @@ API: https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139
 */
 var currlat=0;
 var currlon = 0;
+var currweather;
 $getweather = $('.getweather');
-$getweather.click(function GetLocation (){
+$getweather.click(GetLocation);
+/*Get the longitute and latitude of current location*/
+function GetLocation (){
   if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function(position) {
-   return  currlat =  position.coords.latitude;
-   return currlon = position.coords.longitude;
+    currlat =  position.coords.latitude;
+    currlon = position.coords.longitude;
+/*AJAX*/
+
+    $.ajax({
+        url: 'http://api.openweathermap.org/data/2.5/weather?appid=f5aad87259b289ac34687514827b8dfb' + '&lat=' + currlat + '&lon=' + currlon,
+        data: data,
+        success: function(data){
+            currweather += '<ul>';
+            $.each(data, function(i, main){
+                currweather += '<li><p>Weather: ' + data.main.temp + '</li>';
+            });
+            currweather+= '</ul>';
+            $('#data').html(currweather);
+        }
+
+    })
+
+
+
+
+/*ajax******/
+    
   });
+  
 }
   else {
     alert('Sorry! We could not access your location longitute')
   }
-});
+};
+/*End Get the longitute and latitude of current location*/
+
